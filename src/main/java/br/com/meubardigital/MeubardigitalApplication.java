00f7d369707a3850afc01216bc3adb6cf34d3d91
BrendaDantas <brendasod98@gmail.com>
2025-08-26 16:32:37 -0300

    Estrutura inicial do projeto com Spring Boot.

---
+++ src/main/java/br/com/meubardigital/MeubardigitalApplication.java
@@ -0,0 +1,13 @@
+package br.com.meubardigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeubardigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeubardigitalApplication.class, args);
+	}
+
+}
